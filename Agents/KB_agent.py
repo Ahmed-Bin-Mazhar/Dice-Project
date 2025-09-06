ece8aba60278880706bf75319e33fc6ab9a0d50c
Ahmed Bin Mazhar <ahmedbinmazhar@gmail.com>
2025-09-06 20:21:17 +0000

    edits
    edits

--- Agents/KB_agent.py
+++ Agents/KB_agent.py
@@ -7,7 +7,7 @@ def run_kb_agent(question: str):
         response = requests.post(API_URL, json={"query": question})
         response.raise_for_status()
         print("KB Retrieval process")
-        print(response.json().get("results", "No results found"))
+        # print(response.json().get("results", "No results found"))
         return response.json().get("results", "No results found")  # Return the full JSON from the API
     except requests.RequestException as e:
         return {"error": f"Error contacting KB API: {e}"}
